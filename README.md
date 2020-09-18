4b8598a480b3ea648c0fc90683ed947e23f4c0bc
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-09-17 22:01:43 -0300

    add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Data Sorts 👾
+
+Implementations of most famous data-structures sort algorithms, for study purposes only.
+
+By [Victor B. Fiamoncini](https://github.com/Victor-Fiamoncini) ☕️
--- src/quicksort/index.ts
+++ src/quicksort/index.ts
@@ -1,7 +1,3 @@
-const myArray = [3, 7, 8, 5, 2, 1, 9, 5, 4]
-
-quickSort(myArray, 0, 8)
-
 function quickSort(array: number[], fromIndex: number, toIndex: number) {
 	let countOfMisses = 0
 	let currentCompareIndex = fromIndex
@@ -11,15 +7,18 @@ function quickSort(array: number[], fromIndex: number, toIndex: number) {
 		if (array[currentCompareIndex] > array[pivotIndex]) {
 			swap(array, pivotIndex, pivotIndex - 1)
 
-			if (currentCompareIndex + 1 < pivotIndex)
+			if (currentCompareIndex + 1 < pivotIndex) {
 				swap(array, currentCompareIndex, pivotIndex)
+			}
 
 			pivotIndex--
 		} else {
 			currentCompareIndex++
 			countOfMisses++
 
-			if (countOfMisses == pivotIndex) pivotIndex--
+			if (countOfMisses === pivotIndex) {
+				pivotIndex--
+			}
 		}
 	}
 
@@ -27,17 +26,17 @@ function quickSort(array: number[], fromIndex: number, toIndex: number) {
 	const leftArrayToIndex = pivotIndex - 1
 	const isLeftPivotArrayNotSorted = leftArrayToIndex > 1
 
-	if (isLeftPivotArrayNotSorted)
+	if (isLeftPivotArrayNotSorted) {
 		quickSort(array, leftArrayFromIndex, leftArrayToIndex)
+	}
 
 	const rightArrayFromIndex = pivotIndex + 1
 	const rightArrayToIndex = array.length - 1
 	const isRightPivotArrayNotSorted = rightArrayFromIndex < toIndex
 
-	if (isRightPivotArrayNotSorted)
+	if (isRightPivotArrayNotSorted) {
 		quickSort(array, rightArrayFromIndex, rightArrayToIndex)
-
-	console.log('Array: ', array.toString())
+	}
 }
 
 function swap(array: number[], firstPosition: number, secondPosition: number) {
@@ -46,3 +45,13 @@ function swap(array: number[], firstPosition: number, secondPosition: number) {
 	array[firstPosition] = array[secondPosition]
 	array[secondPosition] = tempElement
 }
+
+function main() {
+	const myArray = [3, 7, 8, 5, 2, 1, 9, 5, 4]
+
+	quickSort(myArray, 0, myArray.length - 1)
+
+	console.log(`Sorted array: ${myArray.join(', ')}`)
+}
+
+main()
